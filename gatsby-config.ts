9263b6ac3e2ac18aa080901096ea18f6887c6ee3
Abhishek Chandrasenan <theshakeabhi@gmail.com>
2023-09-09 14:19:11 +0530

    add chakra ui + make components

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,24 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {number} [portalZIndex=undefined]
+         * The z-index to apply to all portal nodes. This is useful
+         * if your app uses a lot z-index to position elements.
+         */
+        portalZIndex: undefined,
+      },
+    },
+  ],
 }
 
 export default config
